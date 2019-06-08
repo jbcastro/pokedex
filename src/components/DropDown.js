4fa23fa70134365a0be192a3a15c96b879699088
jbcastro <josephbeckcastro@gmail.com>
2019-06-08 12:37:34 -0700

    added the names to the array, going to build a list of types that way. May soon create my own mongoDB and APIs and go from there, the pokemon ones are limited

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -20,23 +20,25 @@ const DropDown = ({ type, handleSelect }) => {
   //       // });
   //     });
   // }
-  const typeFilterId = typeFilter.map(value => {
-    return value.id;
-  });
-  console.log(typeFilterId);
-
-  const gregg = typeFilterId => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${typeFilterId}`)
-      .then(res => res.json())
-      .then(data => {
-        const connie = new BillList(data);
-        console.log(connie);
-      });
+  const setBillList = ({}) => {
+    const typeFilterId = typeFilter.map(value => {
+      return value.id;
+    });
+    const typeFilterName = typeFilter.map(value => {
+      return value.name;
+    });
   };
 
-  if (typeFilterId === !gregg) {
-    console.log("petty");
-  }
+  // console.log(setBillList);
+
+  // const gregg = typeFilterId => {
+  //   fetch(`https://pokeapi.co/api/v2/pokemon/${typeFilterId}`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       const connie = new BillList(data);
+  //       console.log(connie);
+  //     });
+  // };
 
   // console.log(danny.dan);
 
@@ -45,59 +47,38 @@ const DropDown = ({ type, handleSelect }) => {
   console.log("bill " + bill);
 
   return (
-    <div>
-      <li>{gregg}</li>
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Choose Type
-        </Dropdown.Toggle>
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Choose Type
+      </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={type => handleSelect(1)}>
-            Normal
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(2)}>
-            Fighting
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(3)}>
-            Flying
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(4)}>
-            Poison
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(5)}>
-            Ground
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(6)}>Rock </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(7)}>Bug </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(8)}>
-            Ghost{" "}
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(9)}>Steel</Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(10)}>Fire</Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(11)}>
-            Water
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(12)}>
-            Grass
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(13)}>
-            Electric
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(14)}>
-            Psychic
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(15)}>Ice</Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(16)}>
-            Dragon
-          </Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(17)}>Dark</Dropdown.Item>
-          <Dropdown.Item onClick={type => handleSelect(18)}>
-            Fairy
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={type => handleSelect(1)}>Normal</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(2)}>
+          Fighting
+        </Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(3)}>Flying</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(4)}>Poison</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(5)}>Ground</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(6)}>Rock </Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(7)}>Bug </Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(8)}>Ghost </Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(9)}>Steel</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(10)}>Fire</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(11)}>Water</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(12)}>Grass</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(13)}>
+          Electric
+        </Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(14)}>
+          Psychic
+        </Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(15)}>Ice</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(16)}>Dragon</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(17)}>Dark</Dropdown.Item>
+        <Dropdown.Item onClick={type => handleSelect(18)}>Fairy</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
 
