1355005ecc2ac9f5707e7bdf3fdc4413b7b2b03c
jbcastro <josephbeckcastro@gmail.com>
2019-07-11 13:45:03 -0700

    about to redo the whole typeview component, if, or rather when, it breaks come back to this one

--- src/components/TypeView.js
+++ src/components/TypeView.js
@@ -19,70 +19,82 @@ const TypeView = props => {
   //     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
   //     result
   // );
-  const buttass = steve.map(result => butt + result);
-  // console.log(assbutt);
+  const assbutt = steve.map(result => butt + result);
 
-  // const buttass = assbutt.forEach(result => fetch(result));
-
-  // console.log(buttass);
-  // console.log(names);
-
-  // const imgLinks = buttass.map(result => <li key={result} img src={result} />);
-
-  // const typeListItems = names.map(name => <li key={name} />);
-  // console.log(imgLinks);
-  // console.log(typeListItems);
-
-  // const boner = name => (
-  //   <ul className="plain">
-  //     {names.map(name => (
-  //       <li key={name}>{name}</li>
-  //     ))}
-  //     {buttass.map(result => (
-  //       <li key={result}>
-  //         {name}
-  //         <img src={result} />
-  //       </li>
-  //     ))}
-  //   </ul>
-  // );
-
-  // console.log(typeListItems);
-
-  // const homer = buttass.keys(buttass);
-  // console.log(homer);
-  // const merge = { ...names[0], ...buttass[0] };
-  // console.log(merge);
-
-  const marge = Object.keys(buttass);
-  console.log(marge);
-
-  const lisa = Object.keys(names);
-  const maggie = lisa.map(result => result + { names });
-  console.log(maggie);
-
-  const merge = { ...marge, ...buttass };
-  // console.log(merge);
-  // const ginger = merge => (
-  //   <ul className="plain">
-  //     {merge.map(result => (
-  //       <li key={result.id}>{result.names}</li>
-  //     ))}
-  //   </ul>
-  // );
-  // console.log(ginger);
+  // const typeListItems = names.map(name => ({ name }));
+  const imgLinks = assbutt.map(result => <li key={result}>{result}</li>);
 
   return (
     <section className="type-view">
       type: {typeName}
       weak against : {dubDamage0}
-      {/* <ul>
-        {names.map(name => (
-          <TypeView key={name} value={name} />
+      <ul>
+        {names.map(item => (
+          <li key={item}>{item}</li>
         ))}
-      </ul> */}
+      </ul>
     </section>
   );
 };
 
 export default TypeView;
+
+// const buttass = assbutt.forEach(result => fetch(result));
+// console.log(assbutt);
+// console.log(buttass);
+// console.log(names);
+
+// const imgLinks = buttass.map(result => <li key={result} img src={result} />);
+
+// const typeListItems = names.map(name => <li key={name} />);
+// console.log(imgLinks);
+// console.log(typeListItems);
+
+// const boner = name => (
+//   <ul className="plain">
+//     {names.map(name => (
+//       <li key={name}>{name}</li>
+//     ))}
+//     {buttass.map(result => (
+//       <li key={result}>
+//         {name}
+//         <img src={result} />
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+// console.log(typeListItems);
+
+// const homer = buttass.keys(buttass);
+// console.log(homer);
+// const merge = { ...names[0], ...buttass[0] };
+// console.log(merge);
+
+// const marge = Object.keys(buttass);
+// console.log(marge);
+
+// const lisa = Object.keys(names);
+// const maggie = lisa.map(result => result + { names });
+// console.log(maggie);
+
+// const merge = { ...marge, ...buttass };
+// console.log(merge);
+// const ginger = merge => (
+//   <ul className="plain">
+//     {merge.map(result => (
+//       <li key={result.id}>{result.names}</li>
+//     ))}
+//   </ul>
+// );
+// console.log(ginger);
+
+// console.log(imgLinks);
+// const imgLinks2 = (
+//   <ul className="plain">
+//   {imgLinks.map(result=>(
+//     {butt} + result
+//   ))}
+// )
+// console.log(imgLinks2);
+// console.log(imgLinks);
